883198a0f2c6f9baa2d7b699b6bb52e59ddaae94
김영욱 <youngwook.kim@mobin-inc.com>
2021-09-12 20:29:22 +0900

    변수 선언 및 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-    <div></div>
+    <div>
+        <h4>{{ products[0].name }}</h4>
+        <p>{{ products[0].price }} 만원</p>
+    </div>
+    <div>
+        <h4>{{ products[1].name }}</h4>
+        <p>{{ products[1].price }} 만원</p>
+    </div>
+    <div>
+        <h4>{{ products[2].name }}</h4>
+        <p>{{ products[2].price }} 만원</p>
+    </div>
 </template>
 
 <script>
@@ -7,7 +18,13 @@
 export default {
     name: 'App',
     data() {
-        return {}
+        return {
+            products: [
+                {name: '역삼동원룸', price: 60},
+                {name: '천호동원룸', price: 50},
+                {name: '마포구원룸', price: 70}
+            ],
+        }
     },
     components: {}
 }
